7e7522e58b071af4f5cb4acb985e14fffc8a98d0
VKNagibin <itisnotstupid@gmail.com>
2022-07-01 16:04:26 +0300

    добавлен файл gitignore

---
+++ Промисы/async,await.js
@@ -0,0 +1,39 @@
+/*
+
+Перепишите, используя async/await
+    Перепишите один из примеров раздела Цепочка промисов, используя async/await вместо .then/catch:
+
+function loadJson(url) {
+    return fetch(url)
+        .then(response => {
+            if (response.status == 200) {
+                return response.json();
+            } else {
+                throw new Error(response.status);
+            }
+        })
+}
+
+loadJson('no-such-user.json') // (3)
+    .catch(alert); // Error: 404
+
+*/
+
+function loadJson(url) {
+    return fetch(url)
+        .then(response => {
+            if (response.status == 200) {
+                return response.json();
+            } else {
+                throw new Error(response.status);
+            }
+        })
+}
+
+loadJson('no-such-user.json') // (3)
+    .catch(alert); // Error: 404
+
+
+async function loadJson_(url) {
+    let fetching = fetch(url);
+}
