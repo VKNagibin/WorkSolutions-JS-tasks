5a1f4ae00c10508288c911f8d2221eef8cd80588
VKNagibin <itisnotstupid@gmail.com>
2022-07-04 14:32:30 +0300

    add example function

--- example.js
+++ example.js
@@ -1,3 +1,8 @@
 let exampleVar = 1000;
 let exampleArray = [200, -700, 8, 43];
 
+function exampleFunc(num, array) {
+    return [num, ...array];
+}
+
+console.log(exampleFunc(exampleVar, exampleArray));
