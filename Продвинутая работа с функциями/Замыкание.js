721d00b86b349b2a8a617be521c092a009da90a3
Vyacheslav Nagibin <itisnotstupid@gmail.com>
2022-07-17 12:30:39 +0300

    Рефакторинг. Замыкания

--- Продвинутая работа с функциями/Замыкание.js
+++ Продвинутая работа с функциями/Замыкание.js
@@ -42,27 +42,18 @@ sum(100)(250);
 
 let arr = [1, 2, 3, 4, 5, 6, 7];
 
-alert( arr.filter(inBetween(5, 7)) );
-alert( arr.filter(inArray([1, 2, 10])) );
+const inBetween = (min, max) => item => item >= min && item <= max
 
-function inBetween(min, max) {
-    return function(item) {
-        if (item >= min && item <= max) {
+const inArray = array => item => {
+    for (let i of array) {
+        if (item === i) {
             return true;
         }
-        return false
     }
+    return false;
 }
 
-function inArray(array) {
-    return function inArrayInner(item) {
-        for (let i of array) {
-            if (item === i) {
-                return true;
-            }
-        }
-        return false;
-    };
-}
+alert( arr.filter(inBetween(5, 7)) );
+alert( arr.filter(inArray([1, 2, 10])) );
 
 
